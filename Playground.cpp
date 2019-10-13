7eaf03acacf417ac3e6a8a4183ee5477f21fff4d
sif-zia <sifzia@yahoo.com>
2019-10-13 21:21:50 +0500

    Made this program a program that removes spaces from a given string.

--- Playground.cpp
+++ Playground.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main {
-  string st;
+int main() {
+  int n = 0, m = 0;
+  string a,b;
   cout << "Enter a string> ";
-  getline(cin,st)
+  getline(cin,a);
+  while(n<a.length()){
+    If(a[n]!=" "){
+      b[m]=a[n];
+      m++;
+    }
+  }
 
   retrun 0;
-  }
+}
