1c9bfa8e57dc3898dfc6df5a9017135e8ce73c87
sif-zia <sifzia@yahoo.com>
2019-11-03 22:37:04 +0500

    Added ability to do sum.

--- LinearEquationSolver.cpp
+++ LinearEquationSolver.cpp
@@ -48,8 +48,16 @@ bool checkdot(char a){
     }
 }
 
-bool eos(int a, int b){
-    if(b > a){
+bool eos(int fnd, int noint){
+    if(fnd < noint){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+bool seos(int fnd){
+    if(fnd == 0){
         return false;
     }else{
         return true;
@@ -64,23 +72,35 @@ bool eos1(int fnd, int c, int len){
     }
 }
 
+double sttoin(string a){
+    stringstream ss;
+    double b;
+    ss << a;
+    ss >> b;
+    return b;
+}
+
+string intost(float a){
+    stringstream ss;
+    string b;
+    ss << a;
+    ss >> b;
+    return b;
+}
+
 string op(string a, int be, int af){
     int h = 0;
     int leng = a.length();
     double Nu, De, Re;
     string inte, r1, r2 = "";
     size_t found;
-    stringstream s1;
-    stringstream s2;
     stringstream s3;
     found = (leng - af) - 1;
     inte = a.substr(found-be,be);
-    s1 << inte;
-    s1 >> Nu;
+    Nu = sttoin(inte);
     inte = "";
     inte = a.substr(found+1,af);
-    s2 << inte;
-    s2 >> De;
+    De = sttoin(inte);
     if(a[found] == '/'){
         Re = (Nu / De);
     }
@@ -182,13 +202,104 @@ string bre(string a)
     return sr(a);
 }
 
+string sum(string a){
+    int len = a.length(), b = 0 , c = 1 , noint = 1 , e = 0;
+    float x[256], i[256], tx = 0, d = 0, ti = 0;
+    string fx , fi, q;
+    size_t fnd;
+    while (b <= len){
+        fnd = a.find('x',b);
+        if(fnd != string::npos){
+            while((checkint(a[fnd-noint]) || checkdot(a[fnd-noint]) || checkx(a[fnd-noint])) && seos(fnd-(noint-1))){
+                noint++;
+            }
+            noint--;
+            if(noint == 0){
+                x[c] = 1;
+            }else{
+                x[c] = sttoin(a.substr((fnd-noint),noint));
+            }
+            if(a[(fnd-noint)-1] == '-' && !(fnd == 0)){
+                x[c] *= -1;
+            }
+            c++;
+            for(int lo = 0; lo <= noint; lo++){
+                a[fnd-lo] = ' ';
+            }
+            if(a[(fnd-noint)-1] == '+' || a[(fnd-noint)-1] == '-'){
+                a[(fnd-noint)-1] = ' ';
+            }
+            noint = 1;
+        }
+        if(fnd != string::npos){
+            b = fnd+1;
+        }else{
+            a = sr(a);
+            for(int loo = 1; loo <= (c-1); loo++){
+                tx += x[loo];
+            }
+            c = 1;
+            fx = intost(tx);
+            fx += 'x';
+            len = a.length();
+            len--;
+            while(e < len){
+                while(!(checkint(a[d]))){
+                    d++;
+                }
+                noint = 0;
+                while((checkint(a[d+noint]) || checkdot(a[d+noint])) && eos1(d, noint, len)){
+                    noint++;
+                }
+                i[c] = sttoin(a.substr(d,noint));
+                c++;
+                for(int loot = 0; loot < noint; loot++){
+                    a[d-loot] = ' ';
+                }
+                if(a[(d+(noint-1))-1] == '-'){
+                    i[c-1] *= -1;
+                }
+                if(!((d+noint-2) == -1)){
+                    a[(d+(noint-1))-1] = ' ';
+                }
+                e = d+noint;
+                d++;
+            }
+            for(int loop = 1; loop <= (c-1); loop++){
+                ti += i[loop];
+            }
+            fi = intost(ti);
+            if(fi[0] == '-'){
+                q = fx + fi;
+            }else{
+                q = fx + '+' + fi;
+            }
+            b = len + 1;
+        }
+    }
+    return q;
+}
+
+string besum(string f){
+    int found, noint = 0;
+    found = (f.length())-1;
+    while(!(checkint(f[found-noint]) || checkdot(f[found-noint]) || checkx(f[found-noint])) && !eos(found, noint)){
+        noint++;
+    }
+    f = f.substr(0, found-noint-1);
+    return f;
+}
+
 int main()
 {
-    string f = "34x*2+35x/7=45";
-    string g;
-    cout << f  << endl;
-    g = operat(f,'/');
-    cout << g << endl;
-    cout << operat(g,'*') << endl;
+    string f = "4/2x+2*2x-4-2";
+    cout << f << endl;
+    f = operat(f,'/');
+    cout << f << endl;
+    f = operat(f, '*');
+    cout << f << endl;
+    f = besum(f);
+    f = sum(f);
+    cout << f << endl;
     return 0;
 }
